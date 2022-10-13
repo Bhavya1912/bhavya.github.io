e50eb38d80a5b466c000f21610a7b6fdbb2c26bc
BHAVYA TYAGI <btyagi119@gmail.com>
2022-10-13 22:52:50 +0530

    Bhavya's portfolio

---
+++ Portfoliopro/constants/data.js
@@ -0,0 +1,113 @@
+const userData = {
+  githubUsername: "Bhavya1912",
+  name: "Bhavya Tyagi",
+  designation: "Computer Science Student",
+  avatarUrl: "/avatar.png",
+  email: "btyagi119@gmail.com",
+  phone: "+91 9717528360",
+  address: "Ghaziabad, Uttar Pradesh, India.",
+  projects: [
+    {
+      title: "New York Taxi Demand Prediction",
+      link: "https://github.com/Bhavya1912/Yellow-Taxi-Demand-Prediction",
+      imgUrl: "/taxi.jpg",
+    },
+    {
+      title: "ISS Locator",
+      link: "https://github.com/Bhavya1912/ISS-locator-",
+      imgUrl: "/ISS.jpg",
+    },
+
+    {
+      title: "BMI calculator",
+      link: "https://github.com/Bhavya1912/BMI-calculator-",
+      imgUrl: "/1.jpg",
+    },
+    {
+      title: "Basketball Analysis",
+      link: "https://github.com/Bhavya1912/BasketBall-Data-Analysis",
+      imgUrl: "/ss.png",
+    },
+    {
+      title: "Stock Sentiment Analysis",
+      link: "https://github.com/Bhavya1912/Stock-Market-Predictor-",
+      imgUrl: "/sm.png",
+    },
+    {
+      title: "Pokedex App",
+      link: "https://github.com/Bhavya1912/pokedex",
+      imgUrl: "/dribbble.png",
+    },
+    {
+      title: "Feedmeback",
+      link: "https://feedmeback.vercel.app",
+      imgUrl: "/feedmeback.png",
+    },
+    {
+      title: "Language Detection",
+      link: "https://github.com/Bhavya1912/language-detection-fastText",
+      imgUrl: "lan.jpg",
+    },
+  ],
+  about: {
+    title:
+      "I'm a Computer Science Engineering student, loves building products and applications that impact millions of lives",
+    description: [
+      `Innovative Machine Learning Engineering Student working on application
+      design, testing, and deployment experience in writing code as well as building
+      neural networks through programming languages, Possess an unbridled passion
+      for machine learning with comprehensive knowledge of machine learning
+      concepts and other related technologies. Unmatched abilities to identify,
+      understand, and translate program requirements into sustainable, advanced
+      technical solutions through C++, JavaScript, Python, and other programs for
+      continuous improvement of Al technologies.. I'm so out of content right now that I'm literally crying writing this text since lorem text is so 2012 now.`,
+      `I started discovering Web development in my 1st year as I was familiar with HTML,After learning HTML and struggling with CSS, I came up with a brilliant idea of using bootstrap so that I don't have to style everything by myself and - for obvious reasons - if you knew bootstrap, you were cool.`,
+      `But now, I've been coding in React, Angular, Node.js, Express, MongoDB, MySQL, Bootstrap, C, C++,Python,Java, Assembly, Logo and YouDontKnowShit programming language etc to name a few. Although I barely know the syntax (Psst, Stack overflow!), I consider myself a Ninja developer I'm laughing right now.`,
+    ],
+    currentProject: "Machine Learning",
+
+  },
+  experience: [
+    {
+      title: "Campus Ambassador ",
+      company: "verzeo",
+      year: "2022",
+      companyLink: "https://verzeo.com/",
+      desc: " Hosted campus questionnaires to answer queries and resolve doubts students had.",
+    },
+    {
+      title: "Full Stack Web Developer Intern",
+      company: "TwoWaits",
+      year: "2021",
+      companyLink: "http://www.twowaits.in/",
+      desc: "Created innovative websites with animations, audio, and video displays Developed graphics for site pages following branding guidelines And best practices. .",
+    },
+
+
+    {
+      title: "Graduation",
+      company: "IPEC, Ghaziabad",
+      year: "2020-2024",
+      companyLink: "https://www.ipec.org.in/",
+      desc: "Major in Computer Science and Engineering ",
+    },
+    {
+      title: "High School",
+      company: "SRDAV Dayanand Vihar, Delhi",
+      year: "2020",
+      companyLink: "https://upes.co.in",
+      desc: "Subjects being PCM And Computer science",
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1fRCcCPeU0IkVbGRQ023iVQ74f3I8RTS-/view?usp=sharing",
+  socialLinks: {
+    instagram: "https://www.instagram.com/bhxvy.x/",
+    twitter: "https://twitter.com/tweeetlogist",
+    linkedin: "https://www.linkedin.com/in/bhavya19/",
+    github: "https://github.com/Bhavya1912",
+    facebook: "https://facebook.com/",
+  },
+};
+
+export default userData;
